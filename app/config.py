ab330f5b2a09f978341a1c3287fa3b872427641e
Abraham <abrahamnhier97@gmail.com>
2024-10-16 17:06:51 +0300

    file upload

--- app/config.py
+++ app/config.py
@@ -3,9 +3,7 @@ import os
 
 load_dotenv()
 
-
 postgres_uri = os.getenv('POSTGRESQL_URI')
-
 
 if not postgres_uri:
     raise ValueError("No 'POSTGRESQL_URI' set for Flask application. Please set it in the .env file.")
@@ -16,3 +14,15 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES'))
+    
+    # File Upload Settings
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  
+    UPLOAD_EXTENSIONS = ['.pdf', '.doc', '.docx']
+    UPLOAD_PATH = 'uploads/resumes'
+
+    @staticmethod
+    def ensure_upload_path_exists():
+        if not os.path.exists(Config.UPLOAD_PATH):
+            os.makedirs(Config.UPLOAD_PATH)
+
+Config.ensure_upload_path_exists()
